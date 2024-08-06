9a9052431b5ccefe517d089cb33c619df81e8623
codedreammer <nkanal38@gmail.com>
2024-08-06 16:39:43 +0530

    add the code

---
+++ main.py
@@ -0,0 +1,27 @@
+#example1
+a = 4 
+b = "4"
+
+print(a is b) 
+print(a == b)
+
+#example2
+a = 4
+b = 4
+
+print(a == b)
+print(a is b)
+
+#example3
+a = [1,3,45]
+b = [2,4,65]
+
+print(a==b)
+print(a is b)
+
+#example4
+a = None
+b = None
+
+print(a is b)
+print(a is None)
